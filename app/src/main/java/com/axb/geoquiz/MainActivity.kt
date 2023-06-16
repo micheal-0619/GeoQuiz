0e4e7083eefc2fdbeda1f0393e7c9add8328a6ac
Michael_0619 <any_leon@163.com>
2023-06-16 15:19:04 +0800

    [MVC]Use MVC mode, add dynamic display of problem options

--- app/src/main/java/com/axb/geoquiz/MainActivity.kt
+++ app/src/main/java/com/axb/geoquiz/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.View
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.axb.geoquiz.data.Question
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    //增加Question对象集合
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,10 +40,13 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
         trueButton.setOnClickListener {
             Log.d(TAG, "onCreate: trueButton  ")
             val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG)
-            toast.setGravity(Gravity.TOP,0,0)//顶部显示
+            toast.setGravity(Gravity.TOP, 0, 0)//顶部显示
             toast.show()
 
         }
@@ -34,5 +54,14 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener {
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResID = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResID)
+        }
+
+        val questionTextResID = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResID)
     }
 }
